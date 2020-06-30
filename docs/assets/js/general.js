216092e56b2f9a7a309b2f315de808e08d3e7d96
Low Yong Siang <lowyongsiang@gmail.com>
2020-06-30 11:12:58 +0800

    Redesign newsroom

--- docs/assets/js/general.js
+++ docs/assets/js/general.js
@@ -1,3 +1,7 @@
 $(".footer-cat").click(function() {
     $(this).children(".mobile-cross").toggleClass("rotate180");
 })
+
+function customRedirect(link) {
+    window.location.href = link.toString();
+}
