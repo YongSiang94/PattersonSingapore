c8f5737ab51b1b58c144abf16f4c97dcf29e6d73
Low Yong Siang <lowyongsiang@gmail.com>
2021-02-02 11:20:16 +0800

    Modified website
    For redirection only.

--- docs/assets/js/general.js
+++
@@ -1,7 +0,0 @@
-$(".footer-cat").click(function() {
-    $(this).children(".mobile-cross").toggleClass("rotate180");
-})
-
-function customRedirect(link) {
-    window.location.href = link.toString();
-}
